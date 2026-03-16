eeec9f4f438394e33c6ada1a80c88d549c66dbcb
Tony <949962102@qq.com>
2026-03-16 18:42:24 +0800

    Scraping and organizing movie covers from movie websites

---
+++ build.py
@@ -0,0 +1,29 @@
+import os
+
+image_folder = 'image'
+files = os.listdir(image_folder)
+files.sort()
+
+html_content = '''
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Wayne Mansion Private Gallery</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1 class="main-title">THE GOTHAM ARCHIVE</h1> <div class="gallery">
+'''
+
+for f in files:
+    if f.endswith(('.jpg', ',png' ,'.jpeg' ,'webp')):
+        html_content += f'        <div class="poster"><img src="image/{f}"></div>\n'
+
+html_content += f'''
+    </div>
+</body>
+</html>
+'''
+
+with open ('index.html','w',encoding='UTF-8') as A:
+    A.write(html_content)
